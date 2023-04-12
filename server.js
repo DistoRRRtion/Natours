0fdf8514a570c1aecbb223caae24d7430829a2ec
DistoRRRtion <distorrrtion@gmail.com>
2023-04-12 16:18:50 +0300

    added config

--- server.js
+++ server.js
@@ -32,8 +32,16 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLER REJECTION 💥! SUTTING DOWN...');
+  console.log(err.name, err.message);
 
   server.close(() => {
     process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('🖐 SIGTERM RECEIVED. Shutting down grecefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
   });
 });
